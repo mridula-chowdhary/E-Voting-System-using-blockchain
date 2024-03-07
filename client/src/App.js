1919b5ddc67d0e297f3e795b8b6116dadd878809
Mridula Chowdhary <mridulachowdhary21@gmail.com>
2024-03-07 10:59:47 +0530

    Added Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
     <div className="App">
-     <h1 className=''>E-Voting</h1>
+    <Navbar/>
     </div>
   );
 }
